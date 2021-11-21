5b687e9c4657a5a2d43268e0c4079b53e2e4ff03
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-21 11:42:33 +0800

    linking a JS file

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// Linking a JS File
+/* let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN!!");
+
+// num = (40 + 2 - 5) * 10;
+// console.log(num);*/
+
+let js = "amazing";
+console.log((40 + 2 - 5) * 10);
