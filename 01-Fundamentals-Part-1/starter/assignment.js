f378d197a88cfae84f81b923d4a1f953b520332b
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-21 12:27:20 +0800

    Values and Variables Assignment

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+//Lecture: Values and Variables
+let country = "Philippines";
+let continent = "Asia";
+let population = 15000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
