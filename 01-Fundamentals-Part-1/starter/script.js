df7a0830716043a531695eeae745f5cd8a6a6d70
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-21 12:15:52 +0800

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,3 +7,23 @@
 
 let js = "amazing";
 console.log((40 + 2 - 5) * 10);
+
+let firstName = "Jap";
+let PI = 3.1415;
+console.log(firstName);
+console.log(PI);
+
+let myCurrentJob = "Accountant";
+let myDreamJob = "Programmer";
+console.log(myCurrentJob);
+console.log(myDreamJob);
+
+//NOTES:
+
+/*1. Variables should only contain letters, numbers, underscores and dollar sign.
+2. Variables should never start with a number.
+3. For naming convention, variables should always start with smallcase. Uppercase is for OOP's. 
+4. All letters in uppercase are for constants.
+5. There are variables that cannot be used because they are reserved like 'function'. Meaning, they are used by JS for a certain purpose.
+6. JS uses camelCase. 
+7. The word 'name' can be used but discouraged. Use some other term like 'firstName', etc.. */
