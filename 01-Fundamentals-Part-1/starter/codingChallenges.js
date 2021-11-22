949debd56106e489f7a6a742193a323e42d3a522
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-22 20:07:47 +0800

    Coding Challenge 2 done

--- 01-Fundamentals-Part-1/starter/codingChallenges.js
+++ 01-Fundamentals-Part-1/starter/codingChallenges.js
@@ -10,10 +10,9 @@
 // let johnHeight1 = 1.95;
 
 // //Mark and John BMI
-// let markBMI = markMass1 / (markHeight1 * markHeight1);
-// let johnBMI = johnMass1 / johnHeight1 ** 2;
-// console.log(markBMI);
-// console.log(johnBMI);
+// let markBMI = (markMass1 / (markHeight1 * markHeight1)).toFixed(2);
+// let johnBMI = (johnMass1 / johnHeight1 ** 2).toFixed(2);
+// console.log(markBMI, johnBMI);
 
 // //Whether Mark has higher BMI than John
 // let markHigherBMI = markBMI > johnBMI;
@@ -26,10 +25,28 @@
 // let johnHeight2 = 1.76;
 
 // //Mark and John BMI
-// let markBMI2 = markMass2 / markHeight2 ** 2;
-// let johnBMI2 = johnMass2 / johnHeight2 ** 2;
-// console.log(markBMI2);
-// console.log(johnBMI2);
+// let markBMI2 = (markMass2 / markHeight2 ** 2).toFixed(2);
+// let johnBMI2 = (johnMass2 / johnHeight2 ** 2).toFixed(2);
+// console.log(markBMI2, johnBMI2);
 
 // let markHigherBMI2 = markBMI2 > johnBMI2;
 // console.log(markHigherBMI2);
+
+//Coding Challenge 2: If/Else Statement
+// if (markBMI > johnBMI) {
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+// } else {
+//   console.log(
+//     `John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})!`
+//   );
+// }
+
+// if (markBMI2 > johnBMI2) {
+//   console.log(
+//     `Mark's BMI (${markBMI2}) is higher than John's BMI (${johnBMI2})`
+//   );
+// } else {
+//   console.log(
+//     `John's BMI (${johnBMI2}) is higher than Mark's BMI (${markBMI2})!`
+//   );
+// }
