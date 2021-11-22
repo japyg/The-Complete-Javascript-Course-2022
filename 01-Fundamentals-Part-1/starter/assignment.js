80cfd8a131da03f0e7a60ae3f273322ada5c4da1
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-22 22:24:13 +0800

    Switch Statement assignment done

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -98,3 +98,27 @@
 // } else {
 //   console.log(`${country} does not meet your criteria :(`);
 // }
+
+//Lecture: Switch Statement
+// const language = "chinese";
+
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log(`MOST number of native speakers!`);
+//     break;
+//   case "spanish":
+//     console.log(`2nd place in number of native speakers`);
+//     break;
+//   case "english":
+//     console.log(`3rd place`);
+//     break;
+//   case "hindi":
+//     console.log(`Number 4`);
+//     break;
+//   case "arabic":
+//     console.log(`5th most spoken language`);
+//     break;
+//   default:
+//     console.log(`Great language too :D`);
+// }
