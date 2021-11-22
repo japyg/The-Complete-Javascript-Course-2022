b4cee68563d2eadfd16f30a28f4cbbf190dec69e
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-22 20:38:43 +0800

    Type Conversion and Coercion assignment done

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -71,3 +71,10 @@
 //     } million below average`
 //   );
 // }
+
+//Lecture: Type Conversion and Coercion
+// "9" - "5"; 4
+// "19" - "13" + "17"; 617
+// "19" - "13" + 17; 23
+// "123" < 57; false
+// 5 + 6 + "4" + 9 - 4 - 2; 1143
