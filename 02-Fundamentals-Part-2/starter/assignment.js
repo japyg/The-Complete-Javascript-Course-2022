ab7a5f1d58b853ff457995c6fa45b9373ae61dbc
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-23 22:03:12 +0800

    Functions assignment done

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,9 @@
+//Lecture: Functions
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// }
+
+// const pinas = describeCountry("Philippines", 15, "Manila");
+// const sokor = describeCountry("South Korea", 10, "Seoul");
+// const france = describeCountry("France", 8, "Paris");
+// console.log(pinas, sokor, france);
