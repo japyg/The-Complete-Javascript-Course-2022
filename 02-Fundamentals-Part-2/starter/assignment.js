4a53947eda562c2d7457f1e3fc19a17f74bf5672
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-23 22:28:16 +0800

    function declaration and expression assignment done

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -7,3 +7,23 @@
 // const sokor = describeCountry("South Korea", 10, "Seoul");
 // const france = describeCountry("France", 8, "Paris");
 // console.log(pinas, sokor, france);
+
+//Lecture: Function Declaration vs Expression
+
+//function declaration
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// const pinas = percentageOfWorld1(30);
+// const sokor = percentageOfWorld1(20);
+// const china = percentageOfWorld1(1441);
+// console.log(pinas, sokor, china);
+
+//function expression
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+// const pinas = percentageOfWorld2(30);
+// const sokor = percentageOfWorld2(20);
+// const china = percentageOfWorld2(1441);
+// console.log(pinas, sokor, china);
