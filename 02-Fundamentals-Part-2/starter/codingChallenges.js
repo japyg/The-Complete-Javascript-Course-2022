92e14f034889df6a797b6d147c69584d54136425
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-24 22:31:49 +0800

    coding challenge 2 done

--- 02-Fundamentals-Part-2/starter/codingChallenges.js
+++ 02-Fundamentals-Part-2/starter/codingChallenges.js
@@ -23,3 +23,18 @@
 // }
 
 // checkWinner(scoreDolphins, scoreKoalas);
+
+//Coding Challenge #2
+
+// const calcTip = (num) => {
+//   return num >= 50 && num <= 300 ? num * 0.15 : num * 0.2;
+// };
+
+// //test data
+// const bills = [125, 555, 44];
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips);
+
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(total);
