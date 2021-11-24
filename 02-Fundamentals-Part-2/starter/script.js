dd4275a32bf6157b936d2fb3dd795b9e027a427f
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-25 00:02:06 +0800

    add coding challenge in video lecture

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,9 +1,34 @@
-// //Challenge in the video
+// //1st Challenge in the video
 // const jonas = {
 //   firstName: "Jonas",
 //   friends: ["Michael", "Steve", "Peter"],
+//   hasDriversLicense: true,
+//   birthYear: 1991,
+//   job: "teacher",
+
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYear;
+
+//     return this.age;
+//   },
+
+// license: function () {
+//   this.canDrive = this.hasDriversLicense ? "a" : "no";
+//   return this.canDrive;
+// },
+//   getSummary: function () {
+//     return `${this.firstName} is a  ${this.calcAge()}-year old ${
+//       this.job
+//     }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`;
+//   },
 // };
 
 // console.log(
 //   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 // );
+
+//2nd challenge in the video
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+
+// console.log(jonas.getSummary());
