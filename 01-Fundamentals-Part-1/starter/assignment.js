4567f3731eb64fb3baaa00335ea76d2ed92c5f5b
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-25 20:42:30 +0800

    Object Method assignment done

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -162,3 +162,27 @@
 
 // myCountry["population"] -= 2;
 // console.log(myCountry);
+
+//Lecture: Object Methods
+
+// const myCountry = {
+//   country: "Philippines",
+//   capital: "Manila",
+//   language: "Filipino",
+//   population: 15,
+//   neighbours: ["South Korea", "Japan", "Thailand"],
+
+//   describe: function () {
+//     console.log(
+//       ` ${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
+//     );
+//   },
+
+//   checkIsland: function () {
+//     this.isIsland = this.neighbours.length === 0 ? true : false;
+//     return this.isIsland;
+//   },
+// };
+// myCountry.describe();
+// myCountry.checkIsland();
+// console.log(myCountry.isIsland);
