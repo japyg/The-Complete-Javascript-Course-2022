fb63fe2a9efaeb7c3c52e1c3a06f0b1ff50f88be
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-25 22:53:49 +0800

    coding challenge 4 done

--- 02-Fundamentals-Part-2/starter/codingChallenges.js
+++ 02-Fundamentals-Part-2/starter/codingChallenges.js
@@ -70,3 +70,31 @@
 //     mark.BMI < john.BMI ? mark.BMI : john.BMI
 //   })`
 // );
+
+//Coding Challenge 4
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// const calcTip = (num) => {
+//   return num >= 50 && num <= 300 ? num * 0.15 : num * 0.2;
+// };
+
+// for (let i = 0; i < bills.length; i++) {
+//   tips.push(calcTip(bills[i]));
+
+//   totals.push(bills[i] + tips[i]);
+// }
+// console.log(bills, tips, totals);
+
+//Bonus
+
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+
+// console.log(calcAverage(totals));
