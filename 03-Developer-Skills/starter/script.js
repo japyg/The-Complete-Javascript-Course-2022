ca9f0743499cc8d2728dee399caff7448f875bdf
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-26 22:27:08 +0800

    coding challenge in dev skills chapter

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// "use strict";
 
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// // console.log(data1.indexOf(17));
+
+// const printForecast = function (arr) {
+//   let str = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]} degree celsius in ${i}days ...`;
+//   }
+//   console.log(str);
+// };
+// printForecast(data2);
