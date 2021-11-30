9b93e7d441f3c56aedb991e8cb6e0fec6391da59
Jap de Guzman <japphetdeguzman@gmail.com>
2021-11-30 22:51:55 +0800

    THIS keyword in Practice done

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -42,26 +42,64 @@
 // console.log(me);
 // console.log(job);
 // console.log(year);
-var me = 'Jap';
-let job = 'accountant';
-const year = 1992;
+// var me = 'Jap';
+// let job = 'accountant';
+// const year = 1992;
 
-//Functions
+// //Functions
 
-//function declaration
-console.log(addDecl(1, 2));
-// console.log(addExp(1, 2));
-// console.log(addArrow);
-// console.log(addArrow(2,3));
+// //function declaration
+// console.log(addDecl(1, 2));
+// // console.log(addExp(1, 2));
+// // console.log(addArrow);
+// // console.log(addArrow(2,3));
 
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-//function expression
-//if var, the error would be undefined
-const addExpr = function (a, b) {
-  return a + b;
+// //function expression
+// //if var, the error would be undefined
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+//The THIS Keyword in Practice
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
 };
 
-var addArrow = (a, b) => a + b;
+calcAge(1992);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1992);
+
+const jap = {
+  year: 1992,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jap.calcAge();
+
+const matilda = {
+  year: 2000,
+};
+
+matilda.calcAge = jap.calcAge;
+console.log(matilda);
+matilda.calcAge();
+
+const f = jap.calcAge;
+f();
