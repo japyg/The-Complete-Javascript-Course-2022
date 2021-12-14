66de2ba7cf3fd846c0f5949b60adcae869455cbc
Jap de Guzman <japphetdeguzman@gmail.com>
2021-12-14 23:25:22 +0800

    Simple Array Methods done

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,33 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); //shallow copy
+console.log([...arr]); //also a shallow copy
+
+// Splice Method
+console.log(arr.splice(-1));
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//Reverse Method
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]); //same method
+
+//JOIN
+console.log(letters.join(' - '));
