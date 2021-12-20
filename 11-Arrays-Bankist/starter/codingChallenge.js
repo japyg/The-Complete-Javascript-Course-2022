a7f67b5c95b82c7b0e8e071406e8572f2bbbe956
Jap de Guzman <japphetdeguzman@gmail.com>
2021-12-20 23:10:28 +0800

    Coding Challege done

---
+++ 11-Arrays-Bankist/starter/codingChallenge.js
@@ -0,0 +1,41 @@
+// //Data 1:
+// const dogsJulia = [3, 5, 2, 12, 7];
+// const dogsKate = [4, 1, 15, 8, 3];
+
+// const checkDogs = function () {
+//   const dogsJuliaCorrected = dogsJulia.slice(1, 3);
+//   const dogsJK = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogsJK);
+//   dogsJK.forEach(function (dogAge, i) {
+//     if (dogAge >= 3) {
+//       console.log(
+//         `Dog number ${i + 1} is an adult, and is ${dogAge} years old`
+//       );
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy`);
+//     }
+//   });
+// };
+
+// // checkDogs();
+
+// //Data 2:
+// const dogsJulia2 = [9, 16, 6, 8, 3];
+// const dogsKate2 = [10, 5, 6, 1, 4];
+
+// const checkDogs2 = function () {
+//   const dogsJuliaCorrected = dogsJulia2.slice(1, 3);
+//   const dogsJK = dogsJuliaCorrected.concat(dogsKate2);
+//   console.log(dogsJK);
+//   dogsJK.forEach(function (dogAge, i) {
+//     if (dogAge >= 3) {
+//       console.log(
+//         `Dog number ${i + 1} is an adult, and is ${dogAge} years old`
+//       );
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy`);
+//     }
+//   });
+// };
+
+// checkDogs2();
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -69,9 +69,8 @@ const displayMovements = function (movements) {
 
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}
+        </div>
         
         <div class="movements__value">${mov}</div>
       </div>
