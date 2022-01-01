a9695525605837f207cca2c3fbeb9fb5e32d6cd2
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-01 22:27:00 +0800

    Coding Challenge 3 done

--- 11-Arrays-Bankist/starter/codingChallenge.js
+++ 11-Arrays-Bankist/starter/codingChallenge.js
@@ -62,3 +62,18 @@
 
 // calcAverageHumanAge(dogsJulia);
 // calcAverageHumanAge(dogsKate);
+
+// Coding Challenge 3
+const dogsJulia = [5, 2, 4, 1, 15, 8, 3];
+const dogsKate = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = ages => {
+  const average = ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+  console.log(average);
+};
+calcAverageHumanAge(dogsJulia);
+calcAverageHumanAge(dogsKate);
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -127,12 +127,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 
 // Chaining Methods
-const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * eurToUsd)
-  .reduce((acc, mov) => acc + mov);
+// const totalDepositsUSD = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * eurToUsd)
+//   .reduce((acc, mov) => acc + mov);
 
-console.log(totalDepositsUSD);
+// console.log(totalDepositsUSD);
 
 // Map Method
 // const data = [200, 450, -400, 3000, -650, -130, 70, 1300];
