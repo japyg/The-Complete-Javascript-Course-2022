3143fb4f4b34f87330b1696a9e5b07bd4c7e8d85
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-19 22:50:54 +0800

    DOM Traversing done

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -198,3 +198,33 @@ btnScrollTo.addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log('Nav', e.target, e.currentTarget);
 // });
+
+//DOM Traversing
+const h1 = document.querySelector('h1');
+
+//Going downwards: child
+// console.log(document.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'green';
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+const arr = [...h1.parentElement.children];
+console.log(arr);
+arr.forEach(el => {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
