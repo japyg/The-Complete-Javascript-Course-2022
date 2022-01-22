cfd1b406a45a945a32aaaa3ec78e1cb58e37b6c7
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-22 21:33:01 +0800

    Efficient script loading: defer and async done

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -461,13 +461,13 @@ btnScrollTo.addEventListener('click', function (e) {
 // });
 
 //Lifecycle Event
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built!', e);
-});
+// document.addEventListener('DOMContentLoaded', function (e) {
+//   console.log('HTML parsed and DOM tree built!', e);
+// });
 
-window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e);
-});
+// window.addEventListener('load', function (e) {
+//   console.log('Page fully loaded', e);
+// });
 
 // window.addEventListener('beforeunload', function (e) {
 //   e.preventDefault();
