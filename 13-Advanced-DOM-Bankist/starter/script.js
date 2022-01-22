56b98ffb0b3a1bcc8b586411e8823178ba243530
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-22 20:55:31 +0800

    Lifecycle Events done

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -459,3 +459,18 @@ btnScrollTo.addEventListener('click', function (e) {
 // arr.forEach(el => {
 //   if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
+
+//Lifecycle Event
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = 'hello';
+// });
