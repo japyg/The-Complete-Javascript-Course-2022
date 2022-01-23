0bbfaa86d09dcba63e66dd15d24332ef9333f482
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-23 10:03:12 +0800

    Constructor Functions and New Operator done

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//Constructor Function and the New Operator
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never do methods inside a constructor function
+  // this.calcAge = function () {
+  //   console.log(2022 - this.birthYear);
+  // };
+};
+
+const jap = new Person('Jap', 1992);
+console.log(jap);
+
+const chikads = new Person('Chikads', 2013);
+console.log(chikads);
+
+console.log(jap instanceof Person);
