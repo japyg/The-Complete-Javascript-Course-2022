2ea5deef2629018d71bce6c478c45fa87dbf3b2d
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-23 12:54:21 +0800

    Prototypal Inheritance of Built-in Objects done

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -34,3 +34,19 @@ Person.prototype.species = 'Homo Sapiens';
 console.log(jap);
 console.log(jap.hasOwnProperty('firstName'));
 console.log(jap.hasOwnProperty('species'));
+
+//Prototype Chain
+console.log(jap.__proto__);
+console.log(jap.__proto__.__proto__);
+console.log(jap.__proto__.__proto__.__proto__);
+
+//Prototypal Inheritance on Built-in Objects
+const arr = [4, 5, 5, 5, 5, 2];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+//setting a new method in the prototype of array
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
