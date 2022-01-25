5d1dc07dcc609a6931705610e4dc2fa9260418d7
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-25 22:57:46 +0800

    Another class example done

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -193,28 +193,68 @@ Person.prototype.calcAge = function () {
 
 //Inheritance in Object.create
 
-const PersonProto = {
-  calcAge() {
-    console.log(2022 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2022 - this.birthYear);
+//   },
 
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
-const StudentProto = Object.create(PersonProto);
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+// const StudentProto = Object.create(PersonProto);
 
-StudentProto.init = function (firstName, birthYear, course) {
-  PersonProto.init.call(this, firstName, birthYear);
-  this.course = course;
-};
+// StudentProto.init = function (firstName, birthYear, course) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+// };
 
-StudentProto.introduce = function () {
-  console.log(`My name is ${this.firstName} and I am a ${this.course}`);
-};
+// StudentProto.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I am a ${this.course}`);
+// };
 
-const jungkook = Object.create(StudentProto);
-jungkook.init('Jungkook', 1976, 'Gym Coach');
-jungkook.calcAge();
-jungkook.introduce();
+// const jungkook = Object.create(StudentProto);
+// jungkook.init('Jungkook', 1976, 'Gym Coach');
+// jungkook.calcAge();
+// jungkook.introduce();
+
+//Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}!`);
+  }
+
+  //Public Interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved!`);
+    }
+  }
+}
+const acc1 = new Account('Sukjin', 'USD', 1111);
+acc1.deposit(100);
+acc1.withdraw(20);
+
+acc1.requestLoan(300);
+console.log(acc1);
