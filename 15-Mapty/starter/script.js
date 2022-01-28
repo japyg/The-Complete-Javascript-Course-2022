6d0fa3c7b09813a70def040660112a17a81ff62e
Jap de Guzman <japphetdeguzman@gmail.com>
2022-01-28 22:11:48 +0800

    Creating Classes done

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,52 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat, lng]
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+//Testing
+// const run1 = new Running([39, -12], 52, 74, 178);
+// const cycling1 = new Cycling([39, -12], 43, 78, 523);
+
+// console.log(run1, cycling1);
+//////////////////////////////////
+
+//APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -21,7 +67,7 @@ class App {
     //Displays the marker after submitting the form
     form.addEventListener('submit', this._newWorkout.bind(this));
 
-    inputType.addEventListener('change', this._toggleElevationField.bind);
+    inputType.addEventListener('change', this._toggleElevationField);
   }
 
   _getPosition() {
