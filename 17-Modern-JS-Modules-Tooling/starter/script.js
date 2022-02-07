321a0e7788f6c908759c6e3a684a8dbfdd9a6b9f
Jap de Guzman <japphetdeguzman@gmail.com>
2022-02-07 22:17:25 +0800

    Top-Level Await ES2022 done

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -4,12 +4,34 @@
 // addToCart('shoes', 2);
 // console.log(price, totalQty);
 
-console.log('Importing module');
+// console.log('Importing module');
 
 //Importing everything
-// import * as ShoppingCart from './shoppingCart.js';
+import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('shoes', 7);
 // console.log(ShoppingCart);
 
-import add from './shoppingCart.js';
-add('brocolli', 5);
+// import add from './shoppingCart.js';
+// add('brocolli', 5);
+
+//Top-level Await
+
+// console.log('Start fetching...');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log(data[0]);
+
+// console.log('Fetching done');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  //Getting the last item in the array using ES2022
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+//Top-level Await
+const lastPost = await getLastPost();
+console.log(lastPost);
