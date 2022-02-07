c47136bb293a7cb5777b61f613948308d8687863
Jap de Guzman <japphetdeguzman@gmail.com>
2022-02-07 22:33:12 +0800

    Module Pattern done

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,37 +1,68 @@
-//Importing module
-// import { addToCart, totalPrice as price, totalQty } from './shoppingCart.js';
+// //Importing module
+// // import { addToCart, totalPrice as price, totalQty } from './shoppingCart.js';
 
-// addToCart('shoes', 2);
-// console.log(price, totalQty);
+// // addToCart('shoes', 2);
+// // console.log(price, totalQty);
 
-// console.log('Importing module');
+// // console.log('Importing module');
 
-//Importing everything
-import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('shoes', 7);
-// console.log(ShoppingCart);
+// //Importing everything
+// import * as ShoppingCart from './shoppingCart.js';
+// // ShoppingCart.addToCart('shoes', 7);
+// // console.log(ShoppingCart);
 
-// import add from './shoppingCart.js';
-// add('brocolli', 5);
+// // import add from './shoppingCart.js';
+// // add('brocolli', 5);
 
-//Top-level Await
+// //Top-level Await
 
-// console.log('Start fetching...');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// console.log(data[0]);
+// // console.log('Start fetching...');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log(data[0]);
 
-// console.log('Fetching done');
+// // console.log('Fetching done');
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
 
-  //Getting the last item in the array using ES2022
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+//   //Getting the last item in the array using ES2022
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-//Top-level Await
-const lastPost = await getLastPost();
-console.log(lastPost);
+// //Top-level Await
+// const lastPost = await getLastPost();
+// console.log(lastPost);
+
+//The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 10;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('grapes', 3);
+ShoppingCart2.addToCart('milk', 5);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,28 +1,28 @@
-//Exporting module
-console.log('Exporting module');
+// //Exporting module
+// console.log('Exporting module');
 
-//BLOCKING CODE
-console.log('Start fetching users');
-await fetch('https://jsonplaceholder.typicode.com/users');
-console.log('Finish fetching users');
+// //BLOCKING CODE
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
 
-const shippingCost = 10;
-const cart = [];
+// const shippingCost = 10;
+// const cart = [];
 
-export const addToCart = function (product, quantity) {
-  cart.push({ product, quantity });
-
-  console.log(`${quantity} ${product} added to cart`);
-};
-
-const totalPrice = 200;
-const totalQuantity = 100;
-
-export { totalPrice, totalQuantity as totalQty };
-
-// export default function (product, quantity) {
+// export const addToCart = function (product, quantity) {
 //   cart.push({ product, quantity });
 
 //   console.log(`${quantity} ${product} added to cart`);
-// }
-export default addToCart;
+// };
+
+// const totalPrice = 200;
+// const totalQuantity = 100;
+
+// export { totalPrice, totalQuantity as totalQty };
+
+// // export default function (product, quantity) {
+// //   cart.push({ product, quantity });
+
+// //   console.log(`${quantity} ${product} added to cart`);
+// // }
+// export default addToCart;
